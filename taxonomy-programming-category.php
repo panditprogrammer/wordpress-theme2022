546069249bbd04725e4fd62e06142e2b9fc78845
panditprogrammer <panditprogrammer@gmail.com>
2022-04-11 16:02:40 +0530

    custom field

---
+++ taxonomy-programming-category.php
@@ -0,0 +1,45 @@
+<!-- this page will show programming category post  -->
+<?php
+$root = get_template_directory_uri();
+
+get_header();
+$singleCategory = get_queried_object(); //get all category details when click on
+?>
+
+<!-- get all the posts related category  -->
+<link rel="stylesheet" href="<?php echo $root; ?>/custom-post.css">
+<!-- get all custom programming post category wise  -->
+<div class="programming">
+    <h1>Category:<?php echo $singleCategory->name; ?></h1>
+    <div class="post_container">
+        <?php
+        $args = array(
+            'post_type' => 'programming',
+            'post_status' => 'publish',
+            'tax_query' => array(
+                array(
+                    "taxonomy" => "programming-category",
+                    "field" => "term_id",
+                    "terms" => $singleCategory->term_id
+                ),
+            )
+        );
+
+        $wp_query_post = new WP_Query($args);
+
+        while ($wp_query_post->have_posts()) {
+            $wp_query_post->the_post();
+        ?>
+            <div class="post">
+                <h2><?php the_title(); ?></h2>
+                <?php the_post_thumbnail(); ?>
+                <p><?php the_excerpt(); ?></p>
+                <p><?php the_date(); ?></p>
+                <a href="<?php the_permalink(); ?>" class="readmore">Read More</a>
+            </div>
+        <?php
+        } ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
--- template-home.php
+++ template-home.php
@@ -13,14 +13,13 @@ get_header();
     <h1>Blogs </h1>
 
     <?php
-
+    // get category 
     $Categories = get_categories(array("taxonomy" => "category"));
 
     foreach ($Categories as $cate_value) {
     ?>
 
         <div class="col">
-            <h3>ICON 1<h3>
                     <h2><?php echo $cate_value->name; ?></h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem asperiores quidem eius,
                         beatae sequi error modi, eligendi temporibus, sint distinctio quae adipisci?
@@ -32,10 +31,46 @@ get_header();
     }
     ?>
 </section>
+
+<!-- get all programming category  -->
+<section class="programming_category">
+    <h1>All Programming Categories</h1>
+<div class="category">
+    
+<?php
+    // if category has 0 post still show 
+    $programming_category = get_terms(
+        array("taxonomy" => "programming-category", "hide_empty" => false)
+    );
+
+    foreach ($programming_category as $category) {
+        $newCateImage = get_wp_term_image($category->term_id);
+    ?>
+        <div class="category_icon">
+            <?php
+            if ($newCateImage != "") {
+                echo '<img src="' . $newCateImage . '" alt="category image">';
+            } else {
+                echo '<img src="" alt="">';
+            }
+            ?>
+
+           <p> <a href="<?php echo get_category_link($category->term_id); ?>"><?php echo $category->name; ?></a></p>
+        </div>
+    <?php
+
+    }
+    ?>
+</div>
+
+</section>
+
 <link rel="stylesheet" href="<?php echo $root; ?>/custom-post.css">
 <!-- get all custom programming post type  -->
 <div class="programming">
-    <?php
+    <h1>Latest programming posts</h1>
+   <div class="post_container">
+   <?php
     $args = array(
         'post_type' => 'programming',
         'post_status' => 'publish'
@@ -46,7 +81,7 @@ get_header();
         $loop->the_post();
     ?>
         <div class="post">
-            <h1><?php the_title(); ?></h1>
+            <h2><?php the_title(); ?></h2>
             <?php the_post_thumbnail(); ?>
             <p><?php the_excerpt(); ?></p>
             <p><?php the_date(); ?></p>
@@ -54,7 +89,10 @@ get_header();
         </div>
     <?php
     } ?>
+   </div>
 </div>
+
+
 
 <div class="cussection">
     <div class="customeritems">
